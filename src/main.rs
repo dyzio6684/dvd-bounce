0c62ceefbe5de6d0a4052a7824b20decf6ada166
dyzio6684 <51453773+dyzio6684@users.noreply.github.com>
2022-07-01 21:10:30 +0200

    Implement logo bouncing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::{thread, time::Duration};
 
-use sdl2::{pixels::Color, EventPump, event::Event, render::{Canvas, Texture}, video::Window, image::LoadTexture};
+use sdl2::{pixels::Color, EventPump, event::Event, render::{Canvas, Texture}, video::Window, image::LoadTexture, rect::Rect};
+
+const WIN_WIDTH: u32 = 640;
+const WIN_HEIGHT: u32 = 480;
+const IMG_WIDTH: u32 = 96;
+const IMG_HEIGHT: u32 = 64;
+
+struct Data {
+    x: u32,
+    y: u32,
+    rx: bool,
+    ry: bool,
+}
 
 fn run_event_loop(event_pump: &mut EventPump) -> bool {
     for event in event_pump.poll_iter() {
@@ -13,9 +25,20 @@ fn run_event_loop(event_pump: &mut EventPump) -> bool {
     true
 }
 
-fn render(canvas: &mut Canvas<Window>, texture: &mut Texture) {
+fn render(canvas: &mut Canvas<Window>, texture: &mut Texture, data: &mut Data) {
     canvas.clear();
-    canvas.copy(texture, None, None).unwrap();
+
+    data.x = if data.rx { data.x - 1 } else { data.x + 1 };
+    data.y = if data.ry { data.y - 1 } else { data.y + 1 };
+
+    if data.x + IMG_WIDTH >= WIN_WIDTH || data.x <= 0 {
+        data.rx = !data.rx;
+    }
+    if data.y + IMG_HEIGHT >= WIN_HEIGHT || data.y <= 0 {
+        data.ry = !data.ry;
+    }
+
+    canvas.copy(texture, None, Some(Rect::new(data.x as i32, data.y as i32, IMG_WIDTH, IMG_HEIGHT))).unwrap();
     canvas.present();
 }
 
@@ -23,7 +46,7 @@ fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
 
-    let window = video_subsystem.window("DVD Bounce", 640, 480)
+    let window = video_subsystem.window("DVD Bounce", WIN_WIDTH, WIN_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
@@ -38,8 +61,15 @@ fn main() {
     let texture_creator = canvas.texture_creator();
     let mut logo = texture_creator.load_texture("dvd_logo.png").unwrap();
 
+    let mut data = Data {
+        x: 0,
+        y: 0,
+        rx: false,
+        ry: false,
+    };
+
     loop {
-        render(&mut canvas, &mut logo);
+        render(&mut canvas, &mut logo, &mut data);
 
         if !run_event_loop(&mut event_pump) {
             break;
